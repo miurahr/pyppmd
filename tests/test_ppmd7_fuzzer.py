e70e062a9943da6066264b4a0c3d00c16abf9543
Hiroshi Miura <miurahr@linux.com>
2021-04-11 14:46:12 +0900

    Update tests
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- tests/test_ppmd7_fuzzer.py
+++ tests/test_ppmd7_fuzzer.py
@@ -8,17 +8,18 @@ import pyppmd
 MAX_SIZE = min(0xFFFFFFFF - 12 * 3, sys.maxsize)
 
 
-@given(obj=st.binary(min_size=1),
+@given(obj=st.binary(min_size=5),
        max_order=st.integers(min_value=2, max_value=64),
        mem_size=st.integers(min_value=1 << 11, max_value=MAX_SIZE))
 def test_ppmd7_fuzzer(obj, max_order, mem_size):
     result: bytes
-    with pyppmd.Ppmd7Encoder(max_order=max_order, mem_size=mem_size) as enc:
-        result = enc.encode(obj)
-        result += enc.flush()
-    with pyppmd.Ppmd7Decoder(max_order=max_order, mem_size=mem_size) as dec:
-        res = dec.decode(result)
-    assert obj == res
+    enc = pyppmd.Ppmd7Encoder(max_order=max_order, mem_size=mem_size)
+    length = len(obj)
+    result = enc.encode(obj)
+    result += enc.flush()
+    dec = pyppmd.Ppmd7Decoder(max_order=max_order, mem_size=mem_size)
+    res = dec.decode(result, length)
+    assert res == obj
 
 
 if __name__ == "__main__":
