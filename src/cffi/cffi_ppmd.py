afaeca23fac2812e56c02a921b5b2981d8a0e098
Hiroshi Miura <miurahr@linux.com>
2021-04-14 08:28:30 +0900

    Update cffi
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- src/cffi/cffi_ppmd.py
+++ src/cffi/cffi_ppmd.py
@@ -1,5 +1,5 @@
 import sys
-from typing import BinaryIO
+from threading import Lock
 
 from ._cffi_ppmd import ffi, lib
 
@@ -11,23 +11,17 @@ _PPMD7_MAX_ORDER = 64
 _PPMD7_MIN_MEM_SIZE = 1 << 11
 _PPMD7_MAX_MEM_SIZE = 0xFFFFFFFF - 12 * 3
 
-
-@ffi.def_extern()
-def dst_write(b: bytes, size: int, userdata: object) -> None:
-    encoder = ffi.from_handle(userdata)
-    buf = ffi.buffer(b, size)
-    encoder.destination.write(buf)
-
-
-@ffi.def_extern()
-def src_readinto(b: bytes, size: int, userdata: object) -> int:
-    decoder = ffi.from_handle(userdata)
-    buf = ffi.buffer(b, size)
-    result = decoder.source.readinto(buf)
-    return result
-
-
+_BLOCK_SIZE = 16384
 _allocated = []
+
+CFFI_PYPPMD = True
+
+_new_nonzero = ffi.new_allocator(should_clear_after_alloc=False)
+
+
+class PpmdError(Exception):
+    "Call to the underlying library failed."
+    pass
 
 
 @ffi.def_extern()
@@ -45,44 +39,199 @@ def raw_free(o: object) -> None:
         _allocated.remove(o)
 
 
-class Ppmd7Decoder:
-    def __init__(self, destination: BinaryIO, max_order: int, mem_size: int):
+class _BlocksOutputBuffer:
+    KB = 1024
+    MB = 1024 * 1024
+    BUFFER_BLOCK_SIZE = (
+        32*KB, 64*KB, 256*KB, 1*MB, 4*MB, 8*MB, 16*MB, 16*MB,
+        32*MB, 32*MB, 32*MB, 32*MB, 64*MB, 64*MB, 128*MB, 128*MB,
+        256*MB )
+    MEM_ERR_MSG = "Unable to allocate output buffer."
+
+    def initAndGrow(self, out, max_length):
+        # Set & check max_length
+        self.max_length = max_length
+        if 0 <= max_length < self.BUFFER_BLOCK_SIZE[0]:
+            block_size = max_length
+        else:
+            block_size = self.BUFFER_BLOCK_SIZE[0]
+
+        # The first block
+        block = _new_nonzero("char[]", block_size)
+        if block == ffi.NULL:
+            raise MemoryError
+
+        # Create the list
+        self.list = [block]
+
+        # Set variables
+        self.allocated = block_size
+        out.dst = block
+        out.size = block_size
+        out.pos = 0
+
+    def initWithSize(self, out, init_size):
+        # The first block
+        block = _new_nonzero("char[]", init_size)
+        if block == ffi.NULL:
+            raise MemoryError(self.MEM_ERR_MSG)
+
+        # Create the list
+        self.list = [block]
+
+        # Set variables
+        self.allocated = init_size
+        self.max_length = -1
+        out.dst = block
+        out.size = init_size
+        out.pos = 0
+
+    def grow(self, out):
+        # Ensure no gaps in the data
+        assert out.pos == out.size
+
+        # Get block size
+        list_len = len(self.list)
+        if list_len < len(self.BUFFER_BLOCK_SIZE):
+            block_size = self.BUFFER_BLOCK_SIZE[list_len]
+        else:
+            block_size = self.BUFFER_BLOCK_SIZE[-1]
+
+        # Check max_length
+        if self.max_length >= 0:
+            # If (rest == 0), should not grow the buffer.
+            rest = self.max_length - self.allocated
+            assert rest > 0
+
+            # block_size of the last block
+            if block_size > rest:
+                block_size = rest
+
+        # Create the block
+        b = _new_nonzero("char[]", block_size)
+        if b == ffi.NULL:
+            raise MemoryError(self.MEM_ERR_MSG)
+        self.list.append(b)
+
+        # Set variables
+        self.allocated += block_size
+        out.dst = b
+        out.size = block_size
+        out.pos = 0
+
+    def reachedMaxLength(self, out):
+        # Ensure (data size == allocated size)
+        assert out.pos == out.size
+
+        return self.allocated == self.max_length
+
+    def finish(self, out):
+        # Fast path for single block
+        if (len(self.list) == 1 and out.pos == out.size) or \
+           (len(self.list) == 2 and out.pos == 0):
+            return bytes(ffi.buffer(self.list[0]))
+
+        # Final bytes object
+        data_size = self.allocated - (out.size-out.pos)
+        final = _new_nonzero("char[]", data_size)
+        if final == ffi.NULL:
+            raise MemoryError(self.MEM_ERR_MSG)
+
+        # Memory copy
+        # Blocks except the last one
+        posi = 0
+        for block in self.list[:-1]:
+            ffi.memmove(final+posi, block, len(block))
+            posi += len(block)
+        # The last block
+        ffi.memmove(final+posi, self.list[-1], out.pos)
+
+        return bytes(ffi.buffer(final))
+
+class Ppmd7Encoder:
+    def __init__(self,  max_order: int, mem_size: int):
+        self.lock = Lock()
         if mem_size > sys.maxsize:
             raise ValueError("Mem_size exceed to platform limit.")
         if _PPMD7_MIN_ORDER <= max_order <= _PPMD7_MAX_ORDER and _PPMD7_MIN_MEM_SIZE <= mem_size <= _PPMD7_MAX_MEM_SIZE:
             self.closed = False
             self.flushed = False
-            self.destination = destination
             self.ppmd = ffi.new("CPpmd7 *")
             self.rc = ffi.new("CPpmd7z_RangeEnc *")
-            self.writer = ffi.new("RawWriter *")
+            self.writer = ffi.new("BufferWriter *")
             self._allocator = ffi.new("ISzAlloc *")
             self._allocator.Alloc = lib.raw_alloc
             self._allocator.Free = lib.raw_free
-            self._userdata = ffi.new_handle(self)
             lib.ppmd_state_init(self.ppmd, max_order, mem_size, self._allocator)
-            lib.ppmd_compress_init(self.rc, self.writer, lib.dst_write, self._userdata)
+            lib.ppmd_compress_init(self.rc, self.writer)
         else:
             raise ValueError("PPMd wrong parameters.")
 
-    def encode(self, inbuf) -> None:
-        for sym in inbuf:
-            lib.Ppmd7_EncodeSymbol(self.ppmd, self.rc, sym)
+    def encode(self, data) -> bytes:
+        self.lock.acquire()
 
-    def flush(self):
+        # Input buffer
+        in_buf = _new_nonzero("PPMD_inBuffer *")
+        if in_buf == ffi.NULL:
+            raise MemoryError
+        in_buf.src = ffi.from_buffer(data)
+        in_buf.size = len(data)
+        in_buf.pos = 0
+
+        # Output buffer
+        out_buf = _new_nonzero("PPMD_outBuffer *")
+        self.writer.outBuffer = out_buf
+        if out_buf == ffi.NULL:
+            raise MemoryError
+        out = _BlocksOutputBuffer()
+
+        # Initialize output buffer
+        out.initAndGrow(out_buf, -1)
+
+        while True:
+            # Compress
+            ret = lib.ppmd_compress(self.ppmd, self.rc, out_buf, in_buf)
+
+            # Finished
+            if ret == 0:
+                self.lock.release()
+                return out.finish(out_buf)
+
+            # Output buffer should be exhausted, grow the buffer.
+            if out_buf.pos == out_buf.size:
+                out.grow(out_buf)
+
+
+    def flush(self) -> bytes:
         if self.flushed:
             return
         self.flushed = True
+        self.lock.acquire()
+
+        # Output buffer
+        out_buf = _new_nonzero("PPMD_outBuffer *")
+        self.writer.outBuffer = out_buf
+        if out_buf == ffi.NULL:
+            raise MemoryError
+        out = _BlocksOutputBuffer()
+
+        # Initialize output buffer
+        out.initAndGrow(out_buf, -1)
+
         lib.Ppmd7z_RangeEnc_FlushData(self.rc)
+        self.lock.release()
+        return out.finish(out_buf)
 
     def close(self):
         if self.closed:
             return
+        self.lock.acquire()
         self.closed = True
         lib.ppmd_state_close(self.ppmd, self._allocator)
         ffi.release(self.ppmd)
         ffi.release(self.writer)
         ffi.release(self.rc)
+        self.lock.release()
 
     def __enter__(self):
         return self
@@ -93,44 +242,77 @@ class Ppmd7Decoder:
         self.close()
 
 
-class Ppmd7Encoder:
-    def __init__(self, source: BinaryIO, max_order: int, mem_size: int):
-        if not source.readable:
-            raise ValueError("Source stream is not readable")
+class Ppmd7Decoder:
+    def __init__(self, max_order: int, mem_size: int):
         if mem_size > sys.maxsize:
             raise ValueError("Mem_size exceed to platform limit.")
         if _PPMD7_MIN_ORDER <= max_order <= _PPMD7_MAX_ORDER and _PPMD7_MIN_MEM_SIZE <= mem_size <= _PPMD7_MAX_MEM_SIZE:
+            self.lock = Lock()
+            self.lock.acquire()
             self.ppmd = ffi.new("CPpmd7 *")
             self._allocator = ffi.new("ISzAlloc *")
             self._allocator.Alloc = lib.raw_alloc
             self._allocator.Free = lib.raw_free
             lib.ppmd_state_init(self.ppmd, max_order, mem_size, self._allocator)
             self.rc = ffi.new("CPpmd7z_RangeDec *")
-            self.reader = ffi.new("RawReader *")
-            self.source = source  # read indirectly through self._userdata
-            self._userdata = ffi.new_handle(self)
+            self.inBuffer = ffi.new("PPMD_inBuffer *")
+            self.reader = ffi.new("BufferReader *")
             self.closed = False
-            lib.ppmd_decompress_init(self.rc, self.reader, lib.src_readinto, self._userdata)
+            self.inited = False
+            self.lock.release()
         else:
             raise ValueError("PPMd wrong parameters.")
 
-    def decode(self, length) -> bytes:
-        b = bytearray()
-        for _ in range(length):
-            sym = lib.Ppmd7_DecodeSymbol(self.ppmd, self.rc)
-            b += sym.to_bytes(1, "little")
+    def decode(self, data, length) -> bytes:
+        self.lock.acquire()
+
+        # Input buffer
+        in_buf = _new_nonzero("PPMD_inBuffer *")
+        if in_buf == ffi.NULL:
+            raise MemoryError
+        in_buf.src = ffi.from_buffer(data)
+        in_buf.size = len(data)
+        in_buf.pos = 0
+
+        self.reader.inBuffer = in_buf;
+
+        if not self.inited:
+            lib.ppmd_decompress_init(self.rc, self.reader)
+            self.inited = True
+
+        # Output buffer
+        out_buf = _new_nonzero("PPMD_outBuffer *")
+        if out_buf == ffi.NULL:
+            raise MemoryError
+        out = _BlocksOutputBuffer()
+
+        # Initialize output buffer
+        out.initAndGrow(out_buf, -1)
+
+        remaining: int = length
+
+        while remaining > 0:
+            if out_buf.pos == out_buf.size:
+                out.grow(out_buf)
+            size = min(out_buf.size, remaining)
+            lib.ppmd_decompress(self.ppmd, self.rc, out_buf, in_buf, size)
+            remaining = remaining - size
+
         if self.rc.Code != 0:
             pass  # FIXME
-        return bytes(b)
+        self.lock.release()
+        return out.finish(out_buf);
 
     def close(self):
         if self.closed:
             return
+        self.lock.acquire()
         lib.ppmd_state_close(self.ppmd, self._allocator)
         ffi.release(self.ppmd)
         ffi.release(self.reader)
         ffi.release(self.rc)
         self.closed = True
+        self.lock.release()
 
     def __enter__(self):
         return self
--- src/ext/ffi_build.py
+++ src/ext/ffi_build.py
@@ -10,7 +10,7 @@ def is_64bit() -> bool:
 ffibuilder = cffi.FFI()
 
 # ----------- PPMd interfaces ---------------------
-# 7zTypes.h
+# Interface.h
 ffibuilder.cdef(
     r"""
 typedef unsigned char Byte;
@@ -37,6 +37,18 @@ struct ISzAlloc
   void (*Free)(void *address); /* address can be NULL */
 };
 typedef struct ISzAlloc ISzAlloc;
+
+typedef struct PPMD_inBuffer_s {
+    const void* src;    /**< start of input buffer */
+    size_t size;        /**< size of input buffer */
+    size_t pos;         /**< position where reading stopped. Will be updated. Necessarily 0 <= pos <= size */
+} PPMD_inBuffer;
+
+typedef struct PPMD_outBuffer_s {
+    void*  dst;         /**< start of output buffer */
+    size_t size;        /**< size of output buffer */
+    size_t pos;         /**< position where writing stopped. Will be updated. Necessarily 0 <= pos <= size */
+} PPMD_outBuffer;
 """
 )
 
@@ -117,90 +129,31 @@ typedef struct
 """
 )
 
-# Ppmd8.h
-if is_64bit():
-    ffibuilder.cdef("typedef UInt32 CPpmd8_Context_Ref;")
-else:
-    ffibuilder.cdef("typedef struct CPpmd8_Context_ * CPpmd8_Context_Ref;")
-
-ffibuilder.cdef(
-    r"""
-typedef struct CPpmd8_Context_
-{
-  Byte NumStats;
-  Byte Flags;
-  UInt16 SummFreq;
-  CPpmd_State_Ref Stats;
-  CPpmd8_Context_Ref Suffix;
-} CPpmd8_Context;
-"""
-)
-
-ffibuilder.cdef(
-    r"""
-typedef struct
-{
-  CPpmd8_Context *MinContext, *MaxContext;
-  CPpmd_State *FoundState;
-  unsigned OrderFall, InitEsc, PrevSuccess, MaxOrder;
-  Int32 RunLength, InitRL; /* must be 32-bit at least */
-
-  UInt32 Size;
-  UInt32 GlueCount;
-  Byte *Base, *LoUnit, *HiUnit, *Text, *UnitsStart;
-  UInt32 AlignOffset;
-  unsigned RestoreMethod;
-
-  /* Range Coder */
-  UInt32 Range;
-  UInt32 Code;
-  UInt32 Low;
-  union
-  {
-    IByteIn *In;
-    IByteOut *Out;
-  } Stream;
-
-  Byte Indx2Units[38];
-  Byte Units2Indx[128];
-  CPpmd_Void_Ref FreeList[38];
-  UInt32 Stamps[38];
-
-  Byte NS2BSIndx[256], NS2Indx[260];
-  CPpmd_See DummySee, See[24][32];
-  UInt16 BinSumm[25][64];
-} CPpmd8;
-"""
-)
-
 # ----------- python binding API ---------------------
 ffibuilder.cdef(
     r"""
+extern "Python" void *raw_alloc(size_t);
+extern "Python" void raw_free(void *);
 
 typedef struct {
     /* Inherits from IByteOut */
     void (*Write)(void *p, Byte b);
-    void (*dst_write)(char *buf, int size, void *userdata);
-    void *userdata;
-} RawWriter;
+    PPMD_outBuffer *outBuffer;
+} BufferWriter;
 
 typedef struct {
     /* Inherits from IByteIn */
     Byte (*Read)(void *p);
-    int (*src_readinto)(char *buf, int size, void *userdata);
-    void *userdata;
-} RawReader;
-
-extern "Python" int src_readinto(char *, int, void *);
-extern "Python" void dst_write(char *, int, void *);
-
-extern "Python" void *raw_alloc(size_t);
-extern "Python" void raw_free(void *);
+    PPMD_inBuffer *inBuffer;
+} BufferReader;
 
 void ppmd_state_init(CPpmd7 *ppmd, unsigned int maxOrder, unsigned int memSize, ISzAlloc *allocator);
 void ppmd_state_close(CPpmd7 *ppmd, ISzAlloc *allocator);
-int ppmd_decompress_init(CPpmd7z_RangeDec *rc, RawReader *reader, int (*src_readingo)(char *, int, void*), void *userdata);
-void ppmd_compress_init(CPpmd7z_RangeEnc *rc, RawWriter *write, void (*dst_write)(char *, int, void*), void *userdata);
+int ppmd_decompress_init(CPpmd7z_RangeDec *rc, BufferReader *reader);
+void ppmd_compress_init(CPpmd7z_RangeEnc *rc, BufferWriter *write);
+
+int ppmd_compress(CPpmd7 *p, CPpmd7z_RangeEnc *rc, PPMD_outBuffer *out_buf, PPMD_inBuffer *in_buf);
+void ppmd_decompress(CPpmd7 *p, CPpmd7z_RangeDec *rc,PPMD_outBuffer *out_buf, PPMD_inBuffer *in_buf, size_t length);
 
 void Ppmd7_Construct(CPpmd7 *p);
 void Ppmd7_Init(CPpmd7 *p, unsigned maxOrder);
@@ -209,25 +162,11 @@ int Ppmd7_DecodeSymbol(CPpmd7 *p, CPpmd7z_RangeDec *rc);
 void Ppmd7z_RangeEnc_Init(CPpmd7z_RangeEnc *p);
 void Ppmd7z_RangeEnc_FlushData(CPpmd7z_RangeEnc *p);
 void Ppmd7_EncodeSymbol(CPpmd7 *p, CPpmd7z_RangeEnc *rc, int symbol);
-
-void ppmd8_malloc(CPpmd8 *p, unsigned int memSize, ISzAlloc *allocator);
-void ppmd8_mfree(CPpmd8 *ppmd, ISzAlloc *allocator);
-void ppmd8_decompress_init(CPpmd8 *p, RawReader *reader, int (*src_readinto)(char*, int, void*), void *userdata);
-void ppmd8_compress_init(CPpmd8 *p, RawWriter *writer, void (*dst_write)(char*, int, void*), void *userdata);
-
-void Ppmd8_Construct(CPpmd8 *p);
-void Ppmd8_Init(CPpmd8 *p, unsigned maxOrder, unsigned restoreMethod);
-void Ppmd8_EncodeSymbol(CPpmd8 *p, int symbol);
-void Ppmd8_RangeEnc_FlushData(CPpmd8 *p);
-Bool Ppmd8_RangeDec_Init(CPpmd8 *p);
-int Ppmd8_DecodeSymbol(CPpmd8 *p);
 """
 )
 
-# ---------------------------------------------------------------------------
-c_source = r"""
+source = r"""
 #include "Ppmd7.h"
-#include "Ppmd8.h"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -240,30 +179,34 @@ c_source = r"""
 typedef struct {
     /* Inherits from IByteOut */
     void (*Write)(void *p, Byte b);
-    void (*dst_write)(char *buf, int size, void *userdata);
-    void *userdata;
-} RawWriter;
+    PPMD_outBuffer *outBuffer;
+} BufferWriter;
 
 typedef struct {
     /* Inherits from IByteIn */
     Byte (*Read)(void *p);
-    int (*src_readinto)(char *buf, int size, void *userdata);
-    void *userdata;
-} RawReader;
+    PPMD_inBuffer *inBuffer;
+} BufferReader;
 
 static void Write(void *p, Byte b)
 {
-    RawWriter *bw = p;
-    bw->dst_write(&b, 1, bw->userdata);
+    BufferWriter *bw = p;
+    PPMD_outBuffer *buf = bw->outBuffer;
+    if (buf->pos <= buf->size) {
+        *(char *)(buf->dst + buf->pos++) = b;
+    }
 }
 
 static Byte Read(void *p)
 {
-    RawReader *br = p;
+    BufferReader *br = p;
+    PPMD_inBuffer *buf = br->inBuffer;
     char b;
-    int size = br->src_readinto(&b, 1, br->userdata);
-    if (size <= 0)
-        return 0;
+    if (buf->pos <= buf->size) {
+        b = *(char *)(buf->src + buf->pos++);
+    } else {
+        return (Byte) 0;
+    }
     return (Byte) b;
 }
 
@@ -279,59 +222,47 @@ void ppmd_state_close(CPpmd7 *ppmd, ISzAlloc *allocator)
     Ppmd7_Free(ppmd, allocator);
 }
 
-void ppmd_compress_init(CPpmd7z_RangeEnc *rc, RawWriter *writer,
-                        void (*dst_write)(char*, int, void*), void *userdata)
+void ppmd_compress_init(CPpmd7z_RangeEnc *rc, BufferWriter *writer)
 {
     writer->Write = Write;
-    writer->dst_write = dst_write;
-    writer->userdata = userdata;
     rc->Stream = (IByteOut *) writer;
     Ppmd7z_RangeEnc_Init(rc);
 }
 
-int ppmd_decompress_init(CPpmd7z_RangeDec *rc, RawReader *reader,
-                         int (*src_readinto)(char*, int, void*), void *userdata)
+int ppmd_decompress_init(CPpmd7z_RangeDec *rc, BufferReader *reader)
 {
     reader->Read = Read;
-    reader->src_readinto = src_readinto;
-    reader->userdata = userdata;
     rc->Stream = (IByteIn *) reader;
     Bool res = Ppmd7z_RangeDec_Init(rc);
     return res;
 }
 
-void ppmd8_malloc(CPpmd8 *p, unsigned int memSize, ISzAlloc *allocator)
-{
-    Ppmd8_Alloc(p, memSize, allocator);
+int ppmd_compress(CPpmd7 *p, CPpmd7z_RangeEnc *rc, PPMD_outBuffer *out_buf, PPMD_inBuffer *in_buf) {
+    unsigned char* c = (unsigned char *) in_buf->src + in_buf->pos;
+    const unsigned char* in_end = in_buf->src + in_buf->size;
+    while (c < in_end) {
+        Ppmd7_EncodeSymbol(p, rc, *c++);
+        if (out_buf->pos >= out_buf->size) {
+            break;
+        }
+    }
+    in_buf->pos = (void *) c - in_buf->src;
+    return in_buf->size - in_buf->pos;
 }
 
-void ppmd8_mfree(CPpmd8 *ppmd, ISzAlloc *allocator)
-{
-    Ppmd8_Free(ppmd, allocator);
-}
-
-void ppmd8_compress_init(CPpmd8 *p, RawWriter *writer,
-                         void (*dst_write)(char*, int, void*), void *userdata)
-{
-    writer->Write = Write;
-    writer->dst_write = dst_write;
-    writer->userdata = userdata;
-    p->Stream.Out = (IByteOut *) writer;
-}
-
-void ppmd8_decompress_init(CPpmd8 *p, RawReader *reader,
-                         int (*src_readinto)(char*, int, void*), void *userdata)
-{
-    reader->Read = Read;
-    reader->src_readinto = src_readinto;
-    reader->userdata = userdata;
-    p->Stream.In = (IByteIn *) reader;
+void ppmd_decompress(CPpmd7 *p, CPpmd7z_RangeDec *rc,PPMD_outBuffer *out_buf, PPMD_inBuffer *in_buf, size_t length) {
+    unsigned char* c = (unsigned char *) out_buf->dst + out_buf->pos;
+    const unsigned char* out_end = out_buf->dst + length;
+    while (c < out_end) {
+        *c++ = Ppmd7_DecodeSymbol(p, rc);
+    }
+    out_buf->pos = (void *) c - out_buf->dst;
 }
 """
 
 
 def set_kwargs(**kwargs):
-    ffibuilder.set_source(source=c_source, **kwargs)
+    ffibuilder.set_source(source=source, **kwargs)
 
 
 if __name__ == "__main__":  # not when running with setuptools
