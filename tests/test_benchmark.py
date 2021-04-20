f9cb50905c69219883c4c7a8b34d93ec1838be47
Hiroshi Miura <miurahr@linux.com>
2021-04-21 06:49:49 +0900

    PEP8
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- tests/test_benchmark.py
+++ tests/test_benchmark.py
@@ -6,13 +6,13 @@ import pytest
 import pyppmd
 
 testdata_path = pathlib.Path(os.path.join(os.path.dirname(__file__), "data"))
-testdata = testdata_path.joinpath('10000SalesRecords.csv')
+testdata = testdata_path.joinpath("10000SalesRecords.csv")
 src_size = testdata.stat().st_size
 READ_BLOCKSIZE = 16384
 
 targets = [
-    ("PPMd H", 7,  6,  16 << 20),
-   # ("PPMd I", 8, 8, 8 << 20),
+    ("PPMd H", 7, 6, 16 << 20),
+    # ("PPMd I", 8, 8, 8 << 20),
 ]
 
 
@@ -24,7 +24,7 @@ def test_benchmark_text_compress(tmp_path, benchmark, name, var, max_order, mem_
         else:
             encoder = pyppmd.Ppmd8Encoder(max_order=max_order, mem_size=mem_size)
         with tmp_path.joinpath("target.ppmd").open("wb") as target:
-            with testdata.open('rb') as src:
+            with testdata.open("rb") as src:
                 data = src.read(READ_BLOCKSIZE)
                 while len(data) > 0:
                     target.write(encoder.encode(data))
@@ -48,7 +48,7 @@ def test_benchmark_text_decompress(tmp_path, benchmark, name, var, max_order, me
         else:
             decoder = pyppmd.Ppmd8Decoder(max_order=max_order, mem_size=mem_size)
         with tmp_path.joinpath("target.ppmd").open("rb") as src:
-            with tmp_path.joinpath("target.csv").open('wb') as target:
+            with tmp_path.joinpath("target.csv").open("wb") as target:
                 remaining = src_size
                 data = src.read(READ_BLOCKSIZE)
                 while len(data) > 0:
@@ -71,7 +71,7 @@ def test_benchmark_text_decompress(tmp_path, benchmark, name, var, max_order, me
     else:
         encoder = pyppmd.Ppmd8Encoder(max_order=max_order, mem_size=mem_size)
     with tmp_path.joinpath("target.ppmd").open("wb") as target:
-        with testdata.open('rb') as src:
+        with testdata.open("rb") as src:
             data = src.read(READ_BLOCKSIZE)
             while len(data) > 0:
                 target.write(encoder.encode(data))
