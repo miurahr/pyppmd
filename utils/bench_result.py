76a551698b7f9372c1997fa62ae1d445fe87bf2b
Hiroshi Miura <miurahr@linux.com>
2021-04-21 06:54:52 +0900

    CI: update benchmark util
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- utils/bench_result.py
+++ utils/bench_result.py
@@ -19,29 +19,22 @@ def generate_metainfo(root: dict) -> str:
     return result
 
 
-def generate_table(benchmarks: dict, group: str, type='simple') -> str:
+def generate_table(benchmarks: dict, type='simple') -> str:
     base = 1
-    for bm in benchmarks:
-        if group == bm['group']:
-            if bm['params']['name'] == 'lzma2+bcj':
-                base = bm['extra_info']['rate'] / 1000000
-                if base < 0.1:
-                    base = 0.1
     table = []
     for bm in benchmarks:
-        if group == bm['group']:
-            target = bm['params']['name']
-            rate = bm['extra_info']['rate'] / 1000000
-            if rate < 10:
-                rate = round(rate, 2)
-            else:
-                rate = round(rate, 1)
-            ratex = 'x {}'.format(round(rate / base, 2))
-            ratio = round(float(bm['extra_info']['ratio']) * 100, 1)
-            min = bm['stats']['min']
-            max = bm['stats']['max']
-            avr = bm['stats']['mean']
-            table.append([target, rate, ratex, ratio, min, max, avr])
+        target = bm['params']['name']
+        rate = bm['extra_info']['rate'] / 1000000
+        if rate < 10:
+            rate = round(rate, 2)
+        else:
+            rate = round(rate, 1)
+        ratex = 'x {}'.format(round(rate / base, 2))
+        ratio = round(float(bm['extra_info']['ratio']) * 100, 1)
+        min = bm['stats']['min']
+        max = bm['stats']['max']
+        avr = bm['stats']['mean']
+        table.append([target, rate, ratex, ratio, min, max, avr])
     return tabulate(table, headers=['target', 'speed(MB/sec)', 'rate', 'ratio(%)', 'min(sec)', 'max(sec)', 'mean(sec)'],
                     tablefmt=type)
 
@@ -52,10 +45,8 @@ def generate_comment(results_file, type):
     benchmarks = root['benchmarks']
     comment_body = '## Benchmark results\n\n'
     comment_body += generate_metainfo(root)
-    comment_body += '\n\n### Compression benchmarks\n\n'
-    comment_body += generate_table(benchmarks, 'compress', type=type)
-    comment_body += '\n\n### Decompression benchmarks\n\n'
-    comment_body += generate_table(benchmarks, 'decompress', type=type)
+    comment_body += '\n\n### benchmarks\n\n'
+    comment_body += generate_table(benchmarks, type=type)
     return comment_body
 
 
