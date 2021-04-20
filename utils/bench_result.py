f837552af40d5a2c6a43e480b9fc60e672cecfd6
Hiroshi Miura <miurahr@linux.com>
2021-04-21 07:53:46 +0900

    util: reformat by black
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- utils/bench_result.py
+++ utils/bench_result.py
@@ -7,61 +7,66 @@ from tabulate import tabulate  # type: ignore
 
 
 def generate_metainfo(root: dict) -> str:
-    machine_info = root['machine_info']
-    commit_info = root['commit_info']
-    if "brand" in machine_info['cpu']:
+    machine_info = root["machine_info"]
+    commit_info = root["commit_info"]
+    if "brand" in machine_info["cpu"]:
         brand = machine_info["cpu"]["brand"]
     else:
         brand = machine_info["cpu"]["brand_raw"]
-    result = 'Machine: {} {} on {}({})\n'.format(machine_info['system'], machine_info['release'],
-                                                 brand, machine_info['cpu']['hz_actual_friendly'])
-    result += 'Python: {} {} [{} {}]\n'.format(machine_info["python_implementation"],
-                                               machine_info["python_version"],
-                                               machine_info["python_compiler"], machine_info["machine"])
-    result += 'Commit: {} on {} in {}\n'.format(commit_info['id'], commit_info['branch'], commit_info['time'])
+    result = "Machine: {} {} on {}({})\n".format(
+        machine_info["system"], machine_info["release"], brand, machine_info["cpu"]["hz_actual_friendly"]
+    )
+    result += "Python: {} {} [{} {}]\n".format(
+        machine_info["python_implementation"],
+        machine_info["python_version"],
+        machine_info["python_compiler"],
+        machine_info["machine"],
+    )
+    result += "Commit: {} on {} in {}\n".format(commit_info["id"], commit_info["branch"], commit_info["time"])
     return result
 
 
-def generate_table(benchmarks: dict, type='simple') -> str:
+def generate_table(benchmarks: dict, type="simple") -> str:
     base = 1
     table = []
     for bm in benchmarks:
-        target = bm['params']['name']
-        rate = bm['extra_info']['rate'] / 1000000
+        target = bm["params"]["name"]
+        rate = bm["extra_info"]["rate"] / 1000000
         if rate < 10:
             rate = round(rate, 2)
         else:
             rate = round(rate, 1)
-        ratex = 'x {}'.format(round(rate / base, 2))
-        ratio = round(float(bm['extra_info']['ratio']) * 100, 1)
-        min = bm['stats']['min']
-        max = bm['stats']['max']
-        avr = bm['stats']['mean']
+        ratex = "x {}".format(round(rate / base, 2))
+        ratio = round(float(bm["extra_info"]["ratio"]) * 100, 1)
+        min = bm["stats"]["min"]
+        max = bm["stats"]["max"]
+        avr = bm["stats"]["mean"]
         table.append([target, rate, ratex, ratio, min, max, avr])
-    return tabulate(table, headers=['target', 'speed(MB/sec)', 'rate', 'ratio(%)', 'min(sec)', 'max(sec)', 'mean(sec)'],
-                    tablefmt=type)
+    return tabulate(
+        table, headers=["target", "speed(MB/sec)", "rate", "ratio(%)", "min(sec)", "max(sec)", "mean(sec)"], tablefmt=type
+    )
 
 
 def generate_comment(results_file, type):
-    with open(results_file, 'r') as results:
+    with open(results_file, "r") as results:
         root = json.load(results)
-    benchmarks = root['benchmarks']
-    comment_body = '## Benchmark results\n\n'
+    benchmarks = root["benchmarks"]
+    comment_body = "## Benchmark results\n\n"
     comment_body += generate_metainfo(root)
-    comment_body += '\n\n### benchmarks\n\n'
+    comment_body += "\n\n### benchmarks\n\n"
     comment_body += generate_table(benchmarks, type=type)
     return comment_body
 
 
 def main():
-    parser = argparse.ArgumentParser(prog='benchmark_result')
-    parser.add_argument('jsonfile', type=pathlib.Path, help='pytest-benchmark saved result.')
-    parser.add_argument('--markdown', action='store_true', help='print markdown table')
+    parser = argparse.ArgumentParser(prog="benchmark_result")
+    parser.add_argument("jsonfile", type=pathlib.Path, help="pytest-benchmark saved result.")
+    parser.add_argument("--markdown", action="store_true", help="print markdown table")
     args = parser.parse_args()
     if args.markdown:
-        type = 'github'
+        type = "github"
     else:
-        type = 'simple'
+        type = "simple"
     body = generate_comment(args.jsonfile, type)
     print(body)
 
--- utils/github_comment_bench.py
+++ utils/github_comment_bench.py
@@ -7,13 +7,12 @@ import bench_result as bench
 
 
 class CommentBenchmark:
-
     def __init__(self):
-        parser = argparse.ArgumentParser(prog='bench_comment_issue')
-        parser.add_argument('jsonfile', type=pathlib.Path, help='pytest-benchmark saved result')
-        parser.add_argument('repository', help='Repository to post comment')
-        parser.add_argument('issue_number', type=int, help='Issue number to post comment')
-        parser.add_argument('run_id', help='Run ID of github actions')
+        parser = argparse.ArgumentParser(prog="bench_comment_issue")
+        parser.add_argument("jsonfile", type=pathlib.Path, help="pytest-benchmark saved result")
+        parser.add_argument("repository", help="Repository to post comment")
+        parser.add_argument("issue_number", type=int, help="Issue number to post comment")
+        parser.add_argument("run_id", help="Run ID of github actions")
         args = parser.parse_args()
         self.jsonfile = args.jsonfile
         self.repository = args.repository
@@ -21,8 +20,8 @@ class CommentBenchmark:
         self.run_id = args.run_id
 
     def post_comment(self):
-        body = bench.generate_comment(self.jsonfile, type='github')
-        body += '\n   Posted from [the action](https://github.com/{}/actions/runs/{})\n'.format(self.repository, self.run_id)
+        body = bench.generate_comment(self.jsonfile, type="github")
+        body += "\n   Posted from [the action](https://github.com/{}/actions/runs/{})\n".format(self.repository, self.run_id)
         token = os.getenv("GITHUB_TOKEN")
         g = github.Github(token)
         repo = g.get_repo(self.repository)
