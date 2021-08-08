c9e534f3adf3e64d1b9ee0d76999a9eb8b47232c
cielavenir <cielartisan@gmail.com>
2021-08-08 17:11:30 +0900

    Revert "Updated sync method"
    This reverts commit f224a04102da9e596ae7df3212ed37bd6c2b9a9b.

--- lib2/Ppmd8Tdecoder.c
+++ lib2/Ppmd8Tdecoder.c
@@ -3,7 +3,6 @@
 //
 
 #include "Ppmd8Tdecoder.h"
-int inputEmpty = 0;
 
 PPMD_pthread_mutex_t mutex;
 PPMD_pthread_cond_t finished, inEmpty, notEmpty;
@@ -11,7 +10,7 @@ PPMD_pthread_cond_t finished, inEmpty, notEmpty;
 Byte TReader(const void *p) {
     BufferReader *bufferReader = (BufferReader *)p;
     if (bufferReader->inBuffer->pos == bufferReader->inBuffer->size) {
-        inputEmpty = 1;
+        PPMD_pthread_cond_signal(&inEmpty);
         PPMD_pthread_cond_wait(&notEmpty, &mutex);
     }
     return *((const Byte *)bufferReader->inBuffer->src + bufferReader->inBuffer->pos++);
@@ -86,10 +85,8 @@ int Ppmd8T_decode(CPpmd8 *cPpmd8, OutBuffer *out, int max_length, ppmd8_args *ar
     Bool exited = args->finished;
     PPMD_pthread_mutex_unlock(&mutex);
 
-    inputEmpty = 0;
     if (exited) {
         PPMD_pthread_t handle;
-        args->finished = False;
         PPMD_pthread_create(&handle, NULL, Ppmd8T_decode_run, args);
         PPMD_pthread_mutex_lock(&mutex);
         args->handle = handle;
@@ -105,7 +102,6 @@ int Ppmd8T_decode(CPpmd8 *cPpmd8, OutBuffer *out, int max_length, ppmd8_args *ar
         }
     }
     while(True) {
-#if 0
         PPMD_pthread_mutex_lock(&mutex);
         if (PPMD_pthread_cond_wait1(&inEmpty, &mutex) == 0) {
             // inBuffer is empty
@@ -117,11 +113,6 @@ int Ppmd8T_decode(CPpmd8 *cPpmd8, OutBuffer *out, int max_length, ppmd8_args *ar
             break;
         }
         PPMD_pthread_mutex_unlock(&mutex);
-#endif
-	int finished = args->finished;
-	if(inputEmpty)return 0;
-	if(finished)break;
-	usleep(100);
     }
     PPMD_pthread_join(args->handle, NULL);
     return args->result;
