c0e383ff129ba83b2e30c46a1fcbe9f623eb8d16
cielavenir <cielartisan@gmail.com>
2022-03-11 11:48:22 +0900

    reduce MAX_SIZE on 32bit

--- tests/test_fuzzer.py
+++ tests/test_fuzzer.py
@@ -6,8 +6,10 @@ from hypothesis import strategies as st
 
 import pyppmd
 
-MAX_SIZE = 1 << 30
-
+if sys.maxsize > 2**32:
+    MAX_SIZE = 1 << 30
+else:
+    MAX_SIZE = 1 << 28
 
 @given(
     txt=st.text(min_size=1),
