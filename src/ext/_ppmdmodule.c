e7c3ddc5e312d3e3b4f87b9bc2edec7569172987
Hiroshi Miura <miurahr@linux.com>
2022-03-26 01:32:01 +0900

    PPMd8: check double flush
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- src/ext/_ppmdmodule.c
+++ src/ext/_ppmdmodule.c
@@ -1168,7 +1168,7 @@ Ppmd7Encoder_flush(Ppmd7Encoder *self, PyObject *args, PyObject *kwargs)
 
     ACQUIRE_LOCK(self);
     if (self->flushed) {
-        PyErr_SetString(PyExc_ValueError, "Repeated call to flush()");
+        PyErr_SetString(PyExc_ValueError, flush_twice_msg);
         goto error;
     }
 
@@ -1894,6 +1894,10 @@ Ppmd8Encoder_flush(Ppmd8Encoder *self, PyObject *args, PyObject *kwargs)
     }
 
     ACQUIRE_LOCK(self);
+    if (self->flushed) {
+        PyErr_SetString(PyExc_ValueError, flush_twice_msg);
+        goto error;
+    }
 
     if (OutputBuffer_InitAndGrow(&buffer, &out, -1) < 0) {
         PyErr_SetString(PyExc_ValueError, "No memory.");
